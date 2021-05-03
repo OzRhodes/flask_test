c3762e745e1098c9557dc02f4b746933d9f2e33d
oz rhodes <oz@ozrhodes.com>
2021-05-03 07:38:16 +0000

    Debugged

--- RSS.py
+++ RSS.py
@@ -18,9 +18,9 @@ def get_news():
         <i>{1}</i><br>
         <p>{2}</p><br>
     </body>
-    <html>""".format(first_article.get("title""), first_article.get("published"), first_article.get("summary"))
+    <html>'''.format(first_article.get("title""), first_article.get("published"), first_article.get("summary"))
     
-    '''
+   
 
 if __name__ == '__main__':
     app.run(use_reloader=False, debug=True)
