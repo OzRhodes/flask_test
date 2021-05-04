714296b52c6c90cdccd60a9340e3cacbb761632a
Oz Rhodes <oz@ozrhodes.com>
2021-05-04 08:54:04 +0100

    minor format changes to RSS2

--- RSS2.py
+++ RSS2.py
@@ -4,7 +4,7 @@ import feedparser
 from flask import Flask
 app = Flask(__name__)
 
-RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',              'cnn': 'http://rss.cnn.com/rss/edition.rss',              'fox': 'http://feeds.foxnews.com/foxnews/latest',              'iol': 'http://www.iol.co.za/cmlink/1.640'}
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml','cnn': 'http://rss.cnn.com/rss/edition.rss','fox': 'http://feeds.foxnews.com/foxnews/latest'}
 
 
 @app.route("/") 
@@ -18,20 +18,20 @@ def cnn():
 
 @app.route("/fox")
 def fox():
-    return get_news('fox)')
+    return get_news('fox')
 
-def get_news():   
+def get_news(pub):   
     feed = feedparser.parse(RSS_FEEDS[pub])
     first_article = feed['entries'][0]
     
-    return '''<html>
-    <body>
-        <h1> BBC Headlines </h1>
-        <b>{0}</b><br>
-        <i>{1}</i><br>
-        <p>{2}</p><br>
-    </body>
-    <html>'''.format(first_article.get("title""), first_article.get("published"), first_article.get("summary"))
+    return '''<html><head><title>News</title></head>
+<body>
+<h1>{0} Headlines </h1>
+<b>{1}</b><br>
+<i>{2}</i><br>
+<p>{3}</p><br>
+</body>
+<html>'''.format(pub, first_article.get("title"), first_article.get("published"), first_article.get("summary"))
     
 
 if __name__ == '__main__':
