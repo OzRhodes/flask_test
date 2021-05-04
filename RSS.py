df1587e0ab14d2c93dee067d9e7cc854e73299f5
Oz Rhodes <oz@ozrhodes.com>
2021-05-04 08:36:51 +0100

    updated RSS.py

--- RSS.py
+++ RSS.py
@@ -10,15 +10,18 @@ BBC_FEED="http://feeds.bbci.co.uk/news/rss.xml"
 def get_news():   
     feed = feedparser.parse(BBC_FEED)
     first_article = feed['entries'][0]
+    html_string ="""<html>
+<body>
+<h1> BBC Headlines </h1>
+<b>{0}</b> <br/>
+<i>{1}</i> <br/>
+
+        <p>{2}</p> <br/>
+</body>
+</html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+
     
-    return '''<html>
-    <body>
-        <h1> BBC Headlines </h1>
-        <b>{0}</b><br>
-        <i>{1}</i><br>
-        <p>{2}</p><br>
-    </body>
-    <html>'''.format(first_article.get("title""), first_article.get("published"), first_article.get("summary"))
+    return html_string
     
    
 
